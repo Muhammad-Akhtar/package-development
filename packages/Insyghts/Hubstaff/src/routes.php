60114ee5eddb90dbfda83dbde221b9f3139cc807
mac <no-reply@reachme.com>
2022-02-09 11:11:50 +0500

    routes issue fixed, lumen does not support modern routes syntax

--- packages/Insyghts/Hubstaff/src/routes.php
+++ packages/Insyghts/Hubstaff/src/routes.php
@@ -7,11 +7,11 @@ Route::get('hubstaff', function(){
 	echo 'Hello from the hubstaff package!';
 });
 
-Route::post('hubstaff/attendances/filter', [AttendanceController::class, 'attendances']);
-Route::get('hubstaff/attendances', [AttendanceController::class, 'attendances']);
-Route::post('hubstaff/attendance/save', [AttendanceController::class, 'storeAttendanceLog']);
-Route::get('hubstaff/attendance/{id}', [AttendanceController::class, 'showAttendance']);
-Route::post('hubstaff/user-attendance', [AttendanceController::class, 'getAttendanceByUserAndDate']);
-Route::get('hubstaff/attendance/user/{id}', [AttendanceController::class, 'getAttendanceByUser']);
-Route::get('hubstaff/attendance/date/{date}', [AttendanceController::class, 'getAttendanceByDate']);
-Route::post('hubstaff/activity-log/save', [ActivitiesController::class, 'storeActivityLog']);
+Route::post('hubstaff/attendances/filter', 'Insyghts\Hubstaff\Controllers\AttendanceController@attendances');
+Route::get('hubstaff/attendances', 'Insyghts\Hubstaff\Controllers\AttendanceController@attendances');
+Route::post('hubstaff/attendance/save', 'Insyghts\Hubstaff\Controllers\AttendanceController@storeAttendanceLog');
+Route::get('hubstaff/attendance/{id}', 'Insyghts\Hubstaff\Controllers\AttendanceController@showAttendance');
+Route::post('hubstaff/user-attendance', 'Insyghts\Hubstaff\Controllers\AttendanceController@getAttendanceByUserAndDate');
+Route::get('hubstaff/attendance/user/{id}', 'Insyghts\Hubstaff\Controllers\AttendanceController@getAttendanceByUser');
+Route::get('hubstaff/attendance/date/{date}', 'Insyghts\Hubstaff\Controllers\AttendanceController@getAttendanceByDate');
+Route::post('hubstaff/activity-log/save', 'Insyghts\Hubstaff\Controllers\ActivitiesController@storeActivityLog');
